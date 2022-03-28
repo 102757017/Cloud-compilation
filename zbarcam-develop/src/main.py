07d3937ce27fd289daea32f613b984d5722b988e
102757017 <sdf63fg@yeah.net>
2022-03-28 23:15:00 +0800

    a
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- zbarcam-develop/src/main.py
+++ zbarcam-develop/src/main.py
@@ -44,7 +44,8 @@ BoxLayout:
         id: zbarcam
         # optional, by default checks all types
         #code_types: "CODE128","QRCODE"
-        resolution: 1600,1024
+        #这里的分辨率必须填写手机摄像头支持的分辨率，随便填会导致在电脑上可以运行，但是手机app闪退
+        resolution: 2304,1728
         
     Label:
         size_hint: None, None
