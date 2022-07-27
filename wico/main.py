9c4b7a896b8b96526d0033d89bcdf4e0f59c18f0
102757017 <30728579+102757017@users.noreply.github.com>
2022-07-27 12:17:43 +0800

    Update main.py

--- wico/main.py
+++ wico/main.py
@@ -11,7 +11,8 @@ from functools import partial
 from synch import sync_all
 from kivy.logger import Logger
 from pathlib import Path
-from kivy.clock import Clock
+from kivy.core.window import WindowBase
+WindowBase.softinput_mode='below_target'
 
 #  所有基于模块的使用到__file__属性的代码，在源码运行时表示的是当前脚本的绝对路径，但是用pyinstaller打包后就是当前模块的模块名（即文件名xxx.py）
 #  因此需要用以下代码来获取exe的绝对路径
