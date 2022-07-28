3478f93f9524a5fd4ab1459b44316e7b80b4e574
102757017 <30728579+102757017@users.noreply.github.com>
2022-07-28 14:09:22 +0800

    Update main.py

--- wico/main.py
+++ wico/main.py
@@ -34,7 +34,8 @@ if platform == "android":
     from android.permissions import request_permissions, Permission
     request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE, Permission.INTERNET,Permission.CAMERA])    
     from android.storage import primary_external_storage_path
-    appwd=primary_external_storage_path()+'/WicoMag'
+    appwd=primary_external_storage_path()+'/WICO'
+    Logger.info("外部存储路径:"+appwd)
 
 
 
